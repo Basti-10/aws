0a37c360818cd56c8783ca2fa584f7ad3476c418
Bastian Wilbrandt <bastian.wilbrandt@student.uibk.ac.at>
2025-05-08 16:16:34 +0200

    Datum in local format hinzugefügt

--- main.js
+++ main.js
@@ -51,7 +51,8 @@ async function loadStations(url) {
             });
         },
         onEachFeature: function (feature, layer) {
-            console.log(feature.properties);
+            let pointInTime = new Date(feature.properties.date);
+            //console.log(feature.properties);
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
                 <ul>
@@ -60,7 +61,7 @@ async function loadStations(url) {
                 <li>Windgeschwindigkeit (km/h) ${feature.properties.WG || "-"}</li>
                 <li>Schneehöhe (cm) ${feature.properties.HS || "-"}</li>
                 </ul>
-                <span></span>
+                <span>${pointInTime.toLocaleString()}</span>
                
                 `);
         }
